1da29a7e84f9c359f6b793fb76936a1dfd731eef
Phillip dudley <Predatorian3@gmail.com>
2019-02-14 22:15:29 -0600

    Initial Commit

---
+++ lib/minitest/phil_example.rb
@@ -0,0 +1,8 @@
+require "minitest/phil_example/version"
+
+module Minitest
+  module PhilExample
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/minitest/phil_example/version.rb
@@ -0,0 +1,5 @@
+module Minitest
+  module PhilExample
+    VERSION = "0.1.0"
+  end
+end
